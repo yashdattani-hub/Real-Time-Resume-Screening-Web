1e809e3ac3f72e760a774985713ac467328dcc02
Yash Dattani <yashdattani888@gmail.com>
2025-11-19 15:04:43 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pickle
-import re
-
-model = pickel.load(open('knn.pkl','rb'))
-tfid = pickle.load(open('tfidf.pkl','rb'))
-
-st.title("Real Time Resume Screeing Web")
